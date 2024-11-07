043e62ec9828d55f2fa29e57f936d1e34a61abff
Jordan.B <jb.jokod@gmail.com>
2024-11-07 02:00:42 +0100

    👌 IMPROVE: Add PHPDoc on Endpoint parent

--- src/Endpoints/Endpoint.php
+++ src/Endpoints/Endpoint.php
@@ -4,19 +4,27 @@ declare(strict_types = 1);
 
 namespace Jokod\Impactco2Php\Endpoints;
 
+use Jokod\Impactco2Php\Enum\LanguagesEnum;
 use Jokod\Impactco2Php\Interfaces\EndpointInterface;
+use Jokod\Impactco2Php\Exceptions\InvalidArgumentException;
 
 class Endpoint implements EndpointInterface
 {
     /**
-     * @param string $endpoint
-     * @param mixed[] $params
-     * @param mixed[] $query
+     * @param string $endpoint File of the endpoint
+     * @param mixed[] $params Parameters of the endpoint (ex: /{param})
+     * @param mixed[] $query Query parameters of the endpoint (ex: ?param=value)
      */
     public function __construct(private string $endpoint, private array $params = [], private array $query = [])
     {
     }
 
+    /**
+     * Get the path of the endpoint
+     * 
+     * @param string $language Language of the endpoint
+     * @return string
+     */
     public function getPath(string $language): string
     {
         if (!empty($this->params)) {
